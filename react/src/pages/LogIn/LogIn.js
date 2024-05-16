1d6a860c725e8807666f75031a8a77f26c3ccfe5
perez-aaron <acperez5@up.edu.ph>
2024-05-16 15:55:30 +0800

    feat: added navigation to pages

--- react/src/pages/LogIn/LogIn.js
+++ react/src/pages/LogIn/LogIn.js
@@ -1,40 +1,53 @@
-import './LogIn.css';
+import Home from '../Home/Home';
+import { Outlet, Link } from 'react-router-dom';
 
 const LogIn = () => {
 
   const handleLogIn = () => {
+
   };
 
-  const createClick = () =>{
+  function createClick() {
+    const pass = "1234"
+
+    console.log("Successful");
+
+
+    window.location.href = "/home"
+
   };
 
   return (
     <div>
+      <a href="/Home">Home </a>
       <div className="border"></div>
       <div id="SignUpBorder">
-        <form id="SignUpForm" onSubmit={handleLogIn}>
+        <form id="SignUpForm">
           <h1 className="welcome">FARM-TO-TABLE</h1>
           <p className="create">Sign In</p>
 
           <label htmlFor="email" className="email">Email:</label>
-          <input type="email" id="email" name="email" required /><br/>
+          <input type="email" id="email" name="email" required /><br />
 
           <label htmlFor="password" className="password">Password:</label>
-          <input type="password" id="password" name="password" required /><br/>
+          <input type="password" id="password" name="password" required /><br />
+
 
           <button type="submit" className="submit">
-            <p className="submitText">Log In</p>
+            <p className="submitText" value="Submit" onClick={createClick()}>Log In</p>
           </button>
 
           <div className="line"></div>
         </form>
-        <form onSubmit={createClick}>
+
+
+        <form>
           <button type="submit" className="createAcc">
             <p className="createAccText">Create New Account</p>
           </button>
         </form>
       </div>
-    </div>
+    </div >
   );
 }
 
