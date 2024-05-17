1a941787865a39709684bd439b91d984357df7a2
Aaron Perez <159124088+perez-aaron@users.noreply.github.com>
2024-05-17 17:11:29 +0800

    Update index.js

--- react/src/index.js
+++ react/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import LogIn from './pages/LogIn/LogIn';
 import SignUp from './pages/SignUp/SignUp';
 import Home from './pages/Home/Home';
 // import UserList from './pages/UserList/UserList';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <LogIn /> },
+  { path: '/home', element: <Home /> },
+  { path: '/signup', element: <SignUp /> }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <LogIn/>  */}
-    {/* <SignUp/>  */}
-    <Home/> 
-    {/* <UserList/>  */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
