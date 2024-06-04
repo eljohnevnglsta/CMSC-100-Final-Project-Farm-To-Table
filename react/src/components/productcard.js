64af61e01485cea1bdfd108fdabd62ebb99819b3
eljohnevnglsta <112616636+eljohnevnglsta@users.noreply.github.com>
2024-06-04 23:46:38 +0800

    ui: added filter on products (not yet functional)

--- react/src/components/productcard.js
+++ react/src/components/productcard.js
@@ -1,14 +1,28 @@
 export default function ProductCard(props) {
     const product = props.product;
+    const quantity = product.productQuantity;
+    const isOutOfStock = quantity == 0;
     return (
-        <div className="card">
-            <div className="card-content">
-                <img src={product.productImage} alt={product.productName} />
-                <p className="PCProductName">{product.productName}</p>
-                <p>{product.productDescription}</p>
-                <p>{product.productPrice}</p>
-                <button onClick={() => props.addToCart(product)}>Add to Cart</button>
+        (isOutOfStock ? 
+            <div className="card-grayed-out">
+                <div className="card-content">
+                    <img src={product.productImage} alt={product.productName} />
+                    <p className="PCProductName">{product.productName}</p>
+                    <p>{product.productDescription}</p>
+                    <p>{product.productPrice}</p>
+                    <p>Out of Stock</p>
+                </div>
             </div>
-        </div>
+            :
+            <div className="card">
+                <div className="card-content">
+                    <img src={product.productImage} alt={product.productName} />
+                    <p className="PCProductName">{product.productName}</p>
+                    <p>{product.productDescription}</p>
+                    <p>{product.productPrice}</p>
+                    <button onClick={() => props.addToCart(product)}>Add to Cart</button>
+                </div>
+            </div> 
+        )
     );
 }
--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import ProductCard from "../components/productcard";
 import CartItems from "../components/cartitems";    
 import "../stylesheets/Home.css";
+import { set } from "mongoose";
 
 
 const getProducts = async () => {
@@ -58,11 +59,20 @@ export default function Home(props) {
     const [initialCart, setInitialCart] = useState([]);
     const [userData, setUserData] = useState({});
 
-    useEffect(() => {
+    var products = [];
+    var user = {};
+    useState(() => {
         async function fetchData() {
-            const products = await getProducts();
-            const user = await getUserData(email);
-            setProductList(products);
+            //get data from API
+            products = await getProducts();
+            user = await getUserData(email);
+
+            //move out of stock products to the end of array
+            const outOfStock = products.filter(product => product.productQuantity === 0);
+            const inStock = products.filter(product => product.productQuantity > 0);
+            setProductList([...inStock, ...outOfStock]);
+            
+            //set initial cart and user data
             setInitialCart(user.shoppingCart);
             setUserData(user);
         }
@@ -96,12 +106,73 @@ export default function Home(props) {
         setCart((prevCart) => prevCart.filter((item) => item.productId !== productId));
     };
 
+    function handleSorting() {
+        const holder = productList
+        const name = document.querySelector('#name').value;
+        const price = document.querySelector('#price').value;
+        const category = document.querySelector('#category').value;
+
+        var sortedProducts = productList;
+
+        if (name !== 'none') {
+            sortedProducts = sortedProducts.sort((a, b) => {
+                if (name === 'asc') {
+                    return a.productName.localeCompare(b.productName);
+                } else {
+                    return b.productName.localeCompare(a.productName);
+                }
+            });
+        }
+
+        if (price !== 'none') {
+            sortedProducts = sortedProducts.sort((a, b) => {
+                if (price === 'asc') {
+                    return a.productPrice - b.productPrice;
+                } else {
+                    return b.productPrice - a.productPrice;
+                }
+            });
+        }
+
+        if (category !== 'all') {
+            sortedProducts = sortedProducts.filter(product => product.productType === (category === 'poultry' ? 2 : 1));
+        } else {
+            sortedProducts = products;
+        }
+
+        setProductList(sortedProducts);
+    }
+
     return (
         <div id="home-root">
             <Navbar links={navElements} />
             <div className="home-body">
                 <div className="cardsHolder">
-                    <h2>PRODUCTS</h2>
+                    <div className="sort-options">
+                        <p>Name:</p>
+                        <div className="button-group">
+                            <select defaultValue="none" id="name">
+                                <option value="none">Select</option>
+                                <option value="asc">ASC</option>
+                                <option value="low">DESC</option>
+                            </select>
+                        </div>
+                        <p>Price:</p>
+                        <div className="button-group">
+                            <select defaultValue="none" id="price">
+                                <option value="none">Select</option>
+                                <option value="asc">ASC</option>
+                                <option value="low">DESC</option>
+                            </select>
+                        </div>
+                        <p>Category:</p>
+                        <select defaultValue="all" id="category">
+                            <option value="all">All</option>
+                            <option value="poultry">Poultry</option>
+                            <option value="dairy">Crop</option>
+                        </select>
+                        <button id="home-show-all" onClick={handleSorting}>APPLY</button>
+                    </div>
                     <div className="products">
                         {productList.map((product) => (
                             <ProductCard key={product.productId} product={product} addToCart={addToCart} />
