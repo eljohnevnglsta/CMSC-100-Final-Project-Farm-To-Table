29bfba0f09d1f1ed5cb18deef9441830a2affe91
eljohnevnglsta <112616636+eljohnevnglsta@users.noreply.github.com>
2024-06-06 00:43:49 +0800

    chore: only allowed CORS from main port

--- API/MongoDB/index.js
+++ API/MongoDB/index.js
@@ -6,7 +6,11 @@ import cookieParser from 'cookie-parser';
 const app = express();
 app.use(cookieParser());
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.listen(3001, () => { console.log("Database Server Started at Port 3001"); })
